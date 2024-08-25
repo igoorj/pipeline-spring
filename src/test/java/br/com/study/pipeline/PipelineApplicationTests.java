1db417f3d13c680c9a115c55275409e1e93f7793
Igor Goncalves <goncalvesigor67@gmail.com>
2024-08-25 15:21:29 -0300

    first commit

---
+++ src/test/java/br/com/study/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
