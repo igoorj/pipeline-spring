61b57b3d9cb697363ab8306a830b77b0da8bd218
Igor Goncalves <goncalvesigor67@gmail.com>
2024-08-26 19:22:21 -0300

    novo caso de teste

--- src/test/java/br/com/study/pipeline/PipelineApplicationTests.java
+++ src/test/java/br/com/study/pipeline/PipelineApplicationTests.java
@@ -1,5 +1,6 @@
 package br.com.study.pipeline;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class PipelineApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
